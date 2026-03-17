ca379c7e274244d729a512a5281c0fcdb8ccc966
Ralph dos Reis Torres <ralphdosreistorres@gmail.com>
2026-03-16 21:54:05 -0300

    add cards

--- src/SectionTopicos.jsx
+++ src/SectionTopicos.jsx
@@ -15,20 +15,52 @@ const SectionTopicos = () => {
             </p>
           </div>
           <div class="cards">
-            <div class="card-bg">
-              <div>
-              <img
-                src="/src/img/icon-cifrão.svg"
-                width="40px"
-                height="40px"
-                alt=""
-              />
+            <div class="container-card">
+              <img src="./src/img/icon-cifrão.svg" alt="" />
               <h2 class="titulo-card">Captação de Recursos</h2>
               <p class="texto-card">
-                Descubra editais de fomento e programas <br />
-                de aceleração para sua Startup.
+                Descubra editais de fomento e programas de aceleração para sua
+                startup.
               </p>
-              </div>
+            </div>
+            <div class="container-card">
+              <img src="./src/img/icon-ideia.svg" alt="" />
+              <h2 class="titulo-card">Estratégia de Produto</h2>
+              <p class="texto-card">
+                Escolha as funcionalidades certas para validar seu protótipo.
+              </p>
+            </div>
+            <div class="container-card">
+              <img src="./src/img/icon-analise.svg" alt="" />
+              <h2 class="titulo-card">Análise de Mercado</h2>
+              <p class="texto-card">
+                Identifique seus diferenciais competitivos frente aos principais
+                players do mercado.
+              </p>
+            </div>
+            <div class="container-card">
+              <img src="./src/img/icon-cifrão.svg" alt="" />
+              <h2 class="titulo-card">Captação de Recursos</h2>
+              <p class="texto-card">
+                Descubra editais de fomento e programas de aceleração para sua
+                startup.
+              </p>
+            </div>
+            <div class="container-card">
+              <img src="./src/img/icon-cifrão.svg" alt="" />
+              <h2 class="titulo-card">Captação de Recursos</h2>
+              <p class="texto-card">
+                Descubra editais de fomento e programas de aceleração para sua
+                startup.
+              </p>
+            </div>
+            <div class="container-card">
+              <img src="./src/img/icon-cifrão.svg" alt="" />
+              <h2 class="titulo-card">Captação de Recursos</h2>
+              <p class="texto-card">
+                Descubra editais de fomento e programas de aceleração para sua
+                startup.
+              </p>
             </div>
           </div>
         </div>
